8e7ab47c24e8d0c876d29b5388eb895e6f91182e
Eric <eric@system76-pc.localdomain>
2021-09-20 11:07:05 -0500

    add copyright and license

--- uforth.hh
+++ uforth.hh
@@ -1,3 +1,18 @@
+/*
+  The uforth programming language
+  Copyright 2016 Eric J. Deiman
+  This file is part of the uforth programming language.
+  The uforth programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+  The uforth programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  uforth programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #ifndef MAIN_H
 #define MAIN_H
 
